91c3481a0cb7ec522724d4e06effe0b67fd1e974
Sid Meka <gsmekarrow@gmail.com>
2022-02-08 15:26:58 -0500

    Users and authentication routes set up

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const helmet=require("helmet");
 const morgan=require("morgan");
+const userRoute=require("./routes/users");
+const authRoute=require("./routes/auth");
 
 dotenv.config();
 
@@ -16,8 +18,15 @@ app.use(morgan("common"));
 
 app.get("/", (req, res)=>{
     res.send("Welcome to Sidstagram!");
-})
+})//I might have to remove this later!
+
+/*app.get("/users", (req, res)=>{
+    res.send("User Page here!");
+})*/
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, ()=>{
-    console.log("FLIGHT TEAM STAND UP!")
+    console.log("FLIGHT TEAM STAND UP!");
 })
