b8ec738c42cba279337041e06710e7ee47801bcf
Sid Meka <gsmekarrow@gmail.com>
2022-02-08 14:13:18 -0500

    The app prints Welcome to Sidstagram

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const helmet=require("helmet");
+const morgan=require("morgan");
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=>{
+    console.log("MONGODB WORKS!");
+});
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to Sidstagram!");
+})
 
 app.listen(8800, ()=>{
     console.log("FLIGHT TEAM STAND UP!")
