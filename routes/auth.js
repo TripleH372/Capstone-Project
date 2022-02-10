08c34a1138ad042f8dee47e4bc8738abe127a7da
Sid Meka <gsmekarrow@gmail.com>
2022-02-10 08:00:16 -0500

    Added a comment about actual password and changed 400 error to 403.

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@ router.post("/register", async (req, res)=>{//change to post request
         username:req.body.username,
         email:req.body.email,
         password:"**********" //might need to use a hashed password or req.body.password
+        //Might need to save in actualPassword
     });
     //user1.User.deleteMany({}); //Put this back if needed to delete
 
@@ -27,7 +28,7 @@ router.post("/login", async(req, res)=>{
         const user=await User.findOne({email: req.body.email});//might need to change this to username
         !user && res.status(404).json("User not found"); //might need to organize parantheses appropriately
         const validPassword=await bcrypt.compare(req.body.password, user.password);
-        !validPassword && res.status(400).json("Wrong Password!"); //this does actually work properly, but I might need to debug even more down the line
+        !validPassword && res.status(403).json("Wrong Password!"); //this does actually work properly, but I might need to debug even more down the line
     }
     catch(err){
         console.log(err);
