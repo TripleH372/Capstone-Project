87d263818ea8e0c44a92dc53d46ffe5d787e89a7
Sid Meka <gsmekarrow@gmail.com>
2022-02-14 07:10:03 -0500

    Coded Following and Unfollowing Functionality. Added a get request.

--- routes/users.js
+++ routes/users.js
@@ -42,17 +42,50 @@ router.delete("/:id", async (req, res) => {
 router.put("/:id/follow", async(req, res) =>{
     if(req.body.userId===req.params.id) return res.status(403).json("Error: "+req.body.userId+" should not follow himself or herself."); //Might not need to be a return function.
     else{
-        //Code Later
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+              await user.updateOne({ $push: { followers: req.body.userId } });
+              await currentUser.updateOne({ $push: { followings: req.params.id } });
+              res.status(200).json("User has been followed!");
+            } 
+            else res.status(403).json("you allready follow this user");
+        } 
+        catch (err) {
+            res.status(500).json(err);
+        }
     }
 });
 router.put("/:id/unfollow", async(req, res)=>{ //This may be integrated with the follow button or merged with the follow button in another function.
     if(req.body.userId===req.params.id) return res.status(403).json("Error: "+req.body.userId+" cannot unfollow himself or herself."); //Might not need to be a return function.
     else{
-        //Code Later
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) { //Do the exact opposite as above
+              await user.updateOne({ $pull: { followers: req.body.userId } });
+              await currentUser.updateOne({ $pull: { followings: req.params.id } });
+              res.status(200).json("User has been Unfollowed!");
+            } 
+            else res.status(403).json("ERROR: STOP BEING GOOFY!");
+        } 
+        catch (err) {
+            res.status(500).json(err);
+        }
     }
 });
 
-//Should we get a user?
+router.get("/:id", async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other } = user._doc; //Spread operator
+      res.status(200).json(other);
+    } 
+    catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports=router;
